5c8d4eacbaf7fab359cecb7489b182d9c3906d3e
Viskal Bayu <viskal.bayu@gmail.com>
2025-06-03 14:00:50 +0700

    Index UI tweak

--- resources/js/pages/Products/Index.tsx
+++ resources/js/pages/Products/Index.tsx
@@ -1,23 +1,14 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { columns } from '@/components/products/columns';
-import { type Product } from '@/types';
 import { Head, Link, router, useForm } from '@inertiajs/react';
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
 import { Button, buttonVariants } from '@/components/ui/button';
 import { toast } from 'sonner';
 import { PaginationIndex } from '@/components/ui/pagination-index';
 import { DataTable } from '@/components/products/data-table';
 import { FormEventHandler, useRef } from 'react';
 import { Input } from '@/components/ui/input';
+import { SearchIcon, PlusIcon } from 'lucide-react';
 
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -60,18 +51,22 @@ export default function Index({ products }: { products: any }) {
             <Head title="Products Lists" />
             <div className={'mt-8 p-4'}>
                 <Link className={buttonVariants({ variant: 'outline' })} href={`/products/create`}>
+                    <PlusIcon className='w-4 h-4 mr-2' />
                     Create Product
                 </Link>
             </div>
-            <div className={'mt-8 p-4'}>
-                <form onSubmit={filterProduct} className='space-y-6 mt-8 p-4'>
-                    <div className="grid gap-2">
+            <div className={'p-4'}>
+                <form onSubmit={filterProduct} className='space-y-6 mb-4'>
+                    <div className="grid grid-cols-2 gap-2">
                         <Input
                             placeholder="Filter Product"
                             ref={productName}
                             value={data.name}
                             onChange={(e) => setData('name', e.target.value)}
                         />
+                        <Button type="submit" className='cursor-pointer w-[200px]' disabled={processing}>
+                            <SearchIcon className='w-4 h-4 mr-2' /> Search
+                        </Button>
                     </div>
                 </form>
                 <DataTable columns={columns} data={products.data} />
