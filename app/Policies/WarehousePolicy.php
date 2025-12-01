d680df20da71b921f3199502429405f6c024ef3a
viskal <viskal.bayu@gmail.com>
2025-12-01 14:03:03 +0700

    Add Location and Warehouse policies

---
+++ app/Policies/WarehousePolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Warehouse;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class WarehousePolicy
+{
+    use HandlesAuthorization;
+
+    private function hasPermission(User $user, string $ability): bool
+    {
+        return $user->hasPermission("warehouse.{$ability}");
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $this->hasPermission($user, 'viewAny');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Warehouse $warehouse): bool
+    {
+        return $this->hasPermission($user, 'view');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $this->hasPermission($user, 'create');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Warehouse $warehouse): bool
+    {
+        return $this->hasPermission($user, 'update');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Warehouse $warehouse): bool
+    {
+        return $this->hasPermission($user, 'delete');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Warehouse $warehouse): bool
+    {
+        return $this->hasPermission($user, 'restore');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Warehouse $warehouse): bool
+    {
+        return $this->hasPermission($user, 'forceDelete');
+    }
+}
