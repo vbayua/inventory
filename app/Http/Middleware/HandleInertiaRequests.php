0304b4aaa31b054ebf2a29d5fcfa7a01de0f2059
viskal <viskal.bayu@gmail.com>
2025-12-01 14:17:07 +0700

    Expose auth view permissions and route URI

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,14 +38,24 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
-
+        $userIsLoggedIn = $request->user() !== null;
         return [
             ...parent::share($request),
             'name' => config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
+                'viewPermissions' => $userIsLoggedIn ? [
+                    'product' => $request->user()->hasPermission('product.viewAny') ?? false,
+                    'warehouse' => $request->user()->hasPermission('warehouse.viewAny') ?? false,
+                    'location' => $request->user()->hasPermission('location.viewAny') ?? false,
+                    'stock' => $request->user()->hasPermission('stock.viewAny') ?? false,
+                    'operation' => $request->user()->hasPermission('operation.viewAny') ?? false,
+                    'supplier' => $request->user()->hasPermission('supplier.viewAny') ?? false,
+                    'partner' => $request->user()->hasPermission('partner.viewAny') ?? false,
+                ]: []
             ],
+            'uri' => $request->route()?->uri,
             'ziggy' => fn(): array => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
