2c1cb71f49f6cf7dd48b455aec645fd1ae7d4f18
viskal <viskal.bayu@gmail.com>
2025-12-08 08:18:19 +0700

    Expand max width of containe form layout

--- resources/js/components/container-form-layout.tsx
+++ resources/js/components/container-form-layout.tsx
@@ -1,7 +1,3 @@
 export default function ContainerFormLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="rounded-lg mt-12 shadow-md p-6 w-full max-w-4xl mx-auto">
-            {children}
-        </div>
-    );
+    return <div className="mx-auto w-full max-w-5xl rounded-lg p-6 shadow-md">{children}</div>;
 }
