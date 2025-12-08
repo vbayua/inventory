c9324d5f33f12efb2a8547b512df650946a822ae
viskal <viskal.bayu@gmail.com>
2025-12-08 08:58:58 +0700

    Refactor UI language Refactor UI language to Indonesian

--- resources/js/pages/Warehouses/Show.tsx
+++ resources/js/pages/Warehouses/Show.tsx
@@ -1,34 +1,23 @@
 import ContainerLayout from '@/components/container-layout';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link, router } from '@inertiajs/react';
-import { toast } from 'sonner';
-import {
-    Card,
-    CardHeader,
-    CardContent,
-    CardTitle,
-    CardDescription
-} from '@/components/ui/card';
-import {
-    Table,
-    TableHeader,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell
-} from '@/components/ui/table';
 import { Box, ExternalLink, MapPin } from 'lucide-react';
+import { toast } from 'sonner';
 
-export default function Show({ warehouse, stockCount }: {
-    warehouse:
-    {
+export default function Show({
+    warehouse,
+    stockCount,
+}: {
+    warehouse: {
         id: number;
         name?: string;
         locations?: any[];
         products?: any[];
-    },
+    };
     stockCount: string;
 }) {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -39,56 +28,50 @@ export default function Show({ warehouse, stockCount }: {
         {
             title: `${warehouse.name}`,
             href: `/warehouse/${warehouse.id}`,
-        }
+        },
     ];
 
     const deleteWarehouse = (id: number) => {
         if (confirm('are you sure?')) {
-            router.delete(route('warehouse.destroy', { id }))
-            toast.success('Warehouse deleted successfuly')
+            router.delete(route('warehouse.destroy', { id }));
+            toast.success('Warehouse deleted successfuly');
         }
-    }
+    };
 
     const locationCount = warehouse.locations?.length;
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={`${warehouse?.name}`} />
             <ContainerLayout>
-                <Card className='p-2'>
+                <Card className="p-2">
                     <CardHeader>
                         <div className="flex items-start justify-between">
-                            <CardTitle>
-                                {warehouse.name}
-                            </CardTitle>
+                            <CardTitle>{warehouse.name}</CardTitle>
                         </div>
                     </CardHeader>
                     <CardContent>
-                        <div className="grid md:grid-cols-3 gap-6">
+                        <div className="grid gap-6 md:grid-cols-3">
                             <div className="flex items-start gap-3">
-                                <MapPin className='h-5 w-5 text-primary mt-0.5' />
+                                <MapPin className="text-primary mt-0.5 h-5 w-5" />
                                 <div className="">
-                                    <p className="text-sm text-muted-foreground">Locations</p>
-                                    <p className="text-lg mt-2 font-semibold">
-                                        {`${locationCount}`}
-                                    </p>
+                                    <p className="text-muted-foreground text-sm">Lokasi</p>
+                                    <p className="mt-2 text-lg font-semibold">{`${locationCount}`}</p>
                                 </div>
                             </div>
                             <div className="flex items-start gap-3">
-                                <Box className='h-5 w-5 text-primary mt-0.5' />
+                                <Box className="text-primary mt-0.5 h-5 w-5" />
                                 <div className="">
-                                    <p className="text-sm text-muted-foreground">Stock Count</p>
-                                    <p className="text-lg mt-2 font-semibold">
-                                        {`${stockCount}`}
-                                    </p>
+                                    <p className="text-muted-foreground text-sm">Jumlah Item</p>
+                                    <p className="mt-2 text-lg font-semibold">{`${stockCount}`}</p>
                                 </div>
                             </div>
                         </div>
                     </CardContent>
                 </Card>
-                <Card className='p-2'>
+                <Card className="p-2">
                     <CardHeader>
-                        <CardTitle>Locations</CardTitle>
-                        <CardDescription>View and manage locations inside of {`${warehouse.name}`}</CardDescription>
+                        <CardTitle>Daftar Lokasi</CardTitle>
+                        <CardDescription>Lokasi yang terdaftar di {`${warehouse.name}`}</CardDescription>
                     </CardHeader>
                     <CardContent>
                         <Table>
@@ -99,21 +82,21 @@ export default function Show({ warehouse, stockCount }: {
                                 </TableRow>
                             </TableHeader>
                             <TableBody>
-                                {warehouse.locations ? warehouse.locations.map((location) => (
-                                    <TableRow key={location.id}>
-                                        <TableCell>{location.name}</TableCell>
-                                        <TableCell className="text-right">
-                                            <Button
-                                                variant={'ghost'}
-                                                asChild>
-                                                <Link href={route('location.show', location.id)}>
-                                                    View
-                                                    <ExternalLink className='h-3 w-3' />
-                                                </Link>
-                                            </Button>
-                                        </TableCell>
-                                    </TableRow>
-                                )) : (
+                                {warehouse.locations ? (
+                                    warehouse.locations.map((location) => (
+                                        <TableRow key={location.id}>
+                                            <TableCell>{location.name}</TableCell>
+                                            <TableCell className="text-right">
+                                                <Button variant={'ghost'} asChild>
+                                                    <Link href={route('location.show', location.id)}>
+                                                        View
+                                                        <ExternalLink className="h-3 w-3" />
+                                                    </Link>
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
                                     <TableRow>
                                         <TableCell colSpan={2} className="h-26 text-center">
                                             No Results.
