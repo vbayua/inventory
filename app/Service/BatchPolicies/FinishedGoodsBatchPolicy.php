bd0aeb3c244fc94a800b23c0e16423f3dbe96448
Viskal <viskal.bayu@gmail.com>
2026-08-14 09:51:48 +0700

    Update FinishedGoodsBatchPolicy

--- app/Service/BatchPolicies/FinishedGoodsBatchPolicy.php
+++ app/Service/BatchPolicies/FinishedGoodsBatchPolicy.php
@@ -2,10 +2,27 @@
 
 namespace App\Service\BatchPolicies;
 
+use App\Models\Batch;
 use App\Models\Product;
+use Illuminate\Support\Carbon;
 
 class FinishedGoodsBatchPolicy implements BatchPolicyInterface
 {
+    protected $keywords = [
+        'IM',
+        'PE',
+        'RI',
+        'AL',
+        'KOS',
+        'ME',
+        'TI',
+        'KA',
+        'IN',
+        'DO',
+        'NE',
+        'SIA,',
+    ];
+
     public function determineBatch(Product $product, ?int $requestedBatchId = null): ?int
     {
         return $requestedBatchId;
@@ -13,6 +30,22 @@ class FinishedGoodsBatchPolicy implements BatchPolicyInterface
 
     public function generateBatchNumber(Product $product, string $proposedNumber, ?int $supplierId = null, ?string $operationDate): string
     {
+        $opDate = $operationDate ? Carbon::parse($operationDate) : Carbon::now();
+        $year = $opDate->format('Y');
+        $month = $opDate->month;
+
+        $keywordOfTheMonth = $this->keywords[$month - 1];
+
+        $series = Batch::query()
+            ->where('product_id', $product->id)
+            ->whereHas('product.productType', function ($query) {
+                $query->where('name', 'Finished Goods');
+            })
+            ->where('batch_number', 'like', "{$year}{$keywordOfTheMonth}%")
+            ->count();
+
+        $proposedNumber = "{$year}{$proposedNumber}-" . ($series + 1);
+
         return $proposedNumber;
     }
 }
